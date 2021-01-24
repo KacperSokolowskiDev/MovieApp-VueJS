b805176d056c7e1cd8d211fefddaf40da310ead8
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-01-24 22:51:24 +0100

    Created movie app

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  background: purple;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
